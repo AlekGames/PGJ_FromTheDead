f67a8b57c651b6d419762e81a4868e4503f40b3c
AlekGames <alexandre.baccot@gmail.com>
2025-05-14 23:28:53 +0200

    [ADD] Follow behavior + anim logic

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,9 +1,11 @@
 using System;
+
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField]private int maxHealth = 100;
+    [SerializeField] private Animator animatorController; 
     private int currentHealth;
 
     private void Start()
@@ -14,6 +16,7 @@ public class Enemy : MonoBehaviour
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        animatorController.SetTrigger("Hurt");
 
         if (currentHealth <= 0)
         {
