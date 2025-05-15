79374127d35fa6a94d0f5fca7f33a1c6eebf68fb
bonaugur <alainli.perso@gmail.com>
2025-05-15 18:17:17 +0200

    complete core loop hangman+gameover logic

--- Assets/Scripts/Hangman/GameManager.cs
+++ Assets/Scripts/Hangman/GameManager.cs
@@ -3,14 +3,29 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    [Header("Références")]
-    public WordLibrary wordLibrary;
+    public TextMeshProUGUI hintText;
     public GameObject letterSlotPrefab;
     public Transform letterContainer;
 
+
+    [System.Serializable]
+    public class WordWithHint
+    {
+        public string word;
+        public string hint;
+    }
+
+    public WordWithHint[] wordsWithHints;
+
+    // Données internes
+    private WordWithHint currentWord;
     private string wordToGuess;
     private char[] currentGuess;
     private int attemptsLeft = 3;
+
+    private int difficultyLevel = 1;
+
+    private bool isGameOver = false;
 
     void Start()
     {
@@ -19,16 +34,67 @@ public class GameManager : MonoBehaviour
 
     void StartNewGame()
     {
-        wordToGuess = wordLibrary.GetRandomWord();
-        Debug.Log("mot à deviner : " + wordToGuess);
-        currentGuess = new string('_', wordToGuess.Length).ToCharArray();
+        // Choisir un mot aléatoire
+        currentWord = wordsWithHints[Random.Range(0, wordsWithHints.Length)];
+        wordToGuess = currentWord.word.ToUpper();
+        currentGuess = wordToGuess.ToCharArray();
+
+        // Masquer toutes les lettres au départ
+        for (int i = 0; i < currentGuess.Length; i++)
+            currentGuess[i] = '_';
+
+        // Nombre de lettres à révéler selon le niveau
+        int lettersToReveal = wordToGuess.Length - GetLettersToGuessCount();
+
+        // Choisir aléatoirement quelles lettres seront révélées
+        System.Collections.Generic.List<int> indexes = new System.Collections.Generic.List<int>();
+        for (int i = 0; i < wordToGuess.Length; i++) indexes.Add(i);
+
+        for (int i = 0; i < lettersToReveal; i++)
+        {
+            int randomIndex = Random.Range(0, indexes.Count);
+            int revealIndex = indexes[randomIndex];
+            indexes.RemoveAt(randomIndex);
+
+            currentGuess[revealIndex] = wordToGuess[revealIndex];
+        }
+
+
+        // Afficher l’indice
+        if (hintText != null)
+            hintText.text = $"\"{currentWord.hint}\"";
+
+        Debug.Log("Mot à deviner : " + wordToGuess);
+        Debug.Log("indice : " + currentWord.hint);
+        Debug.Log("Difficulté : " + difficultyLevel);
+        Debug.Log("Lettres à deviner : " + GetLettersToGuessCount() + " sur " + wordToGuess.Length);
+
+        // Générer les lettres à l’écran
         GenerateLetterSlots();
-        UpdateDisplayedWord();
+        StartCoroutine(DelayedUpdateDisplayedWord());
     }
+
+    int GetLettersToGuessCount()
+    {
+        int target = 0;
+
+        switch (difficultyLevel)
+        {
+            case 1: target = 2; break;
+            case 2: target = 3; break;
+            case 3: target = 4; break;
+            case 4: target = 5; break;
+            default: target = 5; break;
+        }
+
+        return Mathf.Min(target, wordToGuess.Length);
+    }
+
+
 
     void GenerateLetterSlots()
     {
-        // Supprimer les anciens slots
+        // Nettoyer les anciens slots
         foreach (Transform child in letterContainer)
         {
             Destroy(child.gameObject);
@@ -43,12 +109,23 @@ public class GameManager : MonoBehaviour
 
     void UpdateDisplayedWord()
     {
-        for (int i = 0; i < letterContainer.childCount; i++)
-        {
-            TextMeshProUGUI text = letterContainer.GetChild(i).GetComponentInChildren<TextMeshProUGUI>();
-            text.text = currentGuess[i].ToString();
-        }
+    int count = Mathf.Min(letterContainer.childCount, currentGuess.Length);
+
+    for (int i = 0; i < count; i++)
+    {
+        TextMeshProUGUI text = letterContainer.GetChild(i).GetComponentInChildren<TextMeshProUGUI>();
+        text.text = currentGuess[i].ToString();
     }
+
+    }
+
+    System.Collections.IEnumerator DelayedUpdateDisplayedWord()
+    {
+        yield return null; // attendre une frame
+
+        UpdateDisplayedWord();
+    }
+
 
     public void TryLetter(char letter)
     {
@@ -66,23 +143,40 @@ public class GameManager : MonoBehaviour
         if (!found)
         {
             attemptsLeft--;
-            Debug.Log("Lettre incorrecte. Essais restants : " + attemptsLeft);
+            Debug.Log($"Lettre incorrecte. Essais restants : {attemptsLeft}");
         }
 
         UpdateDisplayedWord();
+        CheckGameEnd();
+    }
 
+    void CheckGameEnd()
+    {
         if (new string(currentGuess) == wordToGuess)
         {
             Debug.Log("Gagné !");
+            
+            // Incrémenter la difficulté (max = 4)
+            if (difficultyLevel < 4)
+                difficultyLevel++;
+
+            // Lancer un nouveau mot avec difficulté augmentée
+            Invoke("StartNewGame", 0.5f); // ou 1f pour une petite pause
         }
+
         else if (attemptsLeft <= 0)
         {
             Debug.Log("Perdu !");
+            ShowGameOver();
         }
     }
 
     void Update()
     {
+        
+        if (isGameOver)
+            return;
+        
         foreach (char c in Input.inputString)
         {
             if (char.IsLetter(c))
@@ -90,5 +184,27 @@ public class GameManager : MonoBehaviour
                 TryLetter(c);
             }
         }
+    }
+
+    void ShowGameOver()
+    {
+        isGameOver = true;
+        
+        string GameOverText = "YOU LOST";
+
+        foreach (Transform child in letterContainer)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (char c in GameOverText)
+        {
+            GameObject slot = Instantiate(letterSlotPrefab, letterContainer);
+            TextMeshProUGUI text = slot.GetComponentInChildren<TextMeshProUGUI>();
+            text.text = c == ' ' ? " " : c.ToString();
+        }
+
+        hintText.text = "you really died now..";
+
     }
 }
